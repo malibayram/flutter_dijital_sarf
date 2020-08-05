7355e88b67990d7d9199189055177d4ecdf23f76
malibayram91 <malibayram91@gmail.com>
2020-08-06 00:57:24 +0300

    adsf

---
+++ lib/veri/hareke.dart
@@ -0,0 +1,13 @@
+class Hareke {
+    static final String ustun = "َ";
+    static final String esre = "ِ";
+    static final String otre = "ُ";
+    static final String ikiUstun = "ً";
+    static final String ikiEsre = "ٍ";
+    static final String ikiOtre = "ٌ";
+    static final String sukun = "ْ";
+    static final String hemzeUstun = "إ";
+    static final String hemzeEsre = "أ";
+    static final String med = "آ";
+    static final String sedde = "ّ";
+}
---
+++ lib/veri/harf.dart
@@ -0,0 +1,36 @@
+class Harf {
+  static final String elif = "ا";
+  static final String be = "ب";
+  static final String te = "ت";
+  static final String se = "ث";
+  static final String cim = "ج";
+  static final String ha = "ح";
+  static final String hI = "خ";
+  static final String dal = "د";
+  static final String zal = "ذ";
+  static final String ra = "ر";
+  static final String zeyn = "ز";
+  static final String sin = "س";
+  static final String shin = "ش";
+  static final String sad = "ص";
+  static final String dat = "ض";
+  static final String tI = "ط";
+  static final String zI = "ظ";
+  static final String ayn = "ع";
+  static final String gayn = "غ";
+  static final String fe = "ف";
+  static final String kaf = "ق";
+  static final String kef = "ك";
+  static final String lam = "ل";
+  static final String mim = "م";
+  static final String nun = "ن";
+  static final String waw = "و";
+  static final String he = "ه";
+  static final String ye = "ي";
+  static final String eHemze = "أ";
+  static final String yHemze = "ئ";
+  static final String hemze = "ء";
+  static final String wHemze = "ؤ";
+  static final String yElif = "ى";
+  static final String yuvarlakTa = "ة";
+}
